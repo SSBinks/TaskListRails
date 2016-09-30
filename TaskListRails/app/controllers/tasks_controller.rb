d66d9f224ee1eb0b84aff1589f87046de6091e8d
Shari Meggs <smeggs1@gmail.com>
2016-09-30 14:57:34 -0700

    Sadly did not git commit throughout project. This is the result of wave three and now I am working on trying to have the date reset if the completion is false

---
+++ TaskListRails/app/controllers/tasks_controller.rb
@@ -0,0 +1,61 @@
+class TasksController < ApplicationController
+  def index
+  @tasks = Task.all
+
+  end
+  def show
+  @task = Task.find(params[:id])
+  end
+  def new
+  @task = Task.new
+  end
+  def create
+    @task = Task.new(task_params)
+    if @task.is_complete == nil ||  @task.is_complete == false
+      @task.update(complete_date:  nil)
+    end
+    if @task.save
+      redirect_to tasks_path
+    else
+      render :new
+    end
+  end
+  def edit
+    @task = Task.find(params[:id])
+  end
+  def update
+    @task = Task.find(params[:id])
+    if @task.is_complete == nil ||  @task.is_complete == false
+      @task.update(complete_date: nil)
+    end
+    if @task.update(task_params)
+      redirect_to tasks_path
+    else
+      render :edit
+    end
+
+  end
+  def complete
+    @task = Task.find(params[:id])
+    if @task.is_complete == nil || @task.is_complete == false
+    @task.update(is_complete: true)
+    @task.update(complete_date: Date.today)
+    redirect_to task_path
+    else
+    redirect_to task_path
+    end
+
+  end
+  def destroy
+    Task.find(params[:id]).destroy
+  redirect_to tasks_path
+
+  end
+  private
+  def task_params
+    params.require(:task).permit(:task_name, :description, :is_complete, :complete_date)
+  end
+  def update_complete_status_params
+    params.require(:task).permit( :is_complete)
+  end
+end

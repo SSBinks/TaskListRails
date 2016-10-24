13eca2ee335e97e6a71a437e351cdd2018c2fd42
Shari Meggs <smeggs1@gmail.com>
2016-10-23 18:03:55 -0700

    fixed some errors in the controller

--- TaskListRails/app/controllers/sessions_controller.rb
+++ TaskListRails/app/controllers/sessions_controller.rb
@@ -1,33 +1,25 @@
 class SessionsController < ApplicationController
 
-    def login_failure; end
+  def login_failure; end
 
-    def login; end
+  def login; end
 
-    def create
-        auth_hash = request.env['omniauth.auth']
-        redirect_to login_failure_path unless auth_hash['uid']
-        @task = Task.find_by(uid: auth_hash[:uid], provider: 'github')
-        if @task.nil?
-          @task = Task.build_from_github(auth_hash)
-          render :login_failure unless @task.save
-        end
-
-        session[:merchant_id] = @task.id
-        redirect_to sessions_path
+  def create
+    auth_hash = request.env['omniauth.auth']
+    redirect_to login_failure_path unless auth_hash['uid']
+    @user = User.find_by(uid: auth_hash[:uid], provider: 'github')
+    if @user.nil?
+      # User doesn't match anything in the DB.
+      # Attempt to create a new user.
+      @user = User.build_from_github(auth_hash)
+      render :creation_failure unless @user.save
     end
 
-    def index
-        if session[:merchant_id].nil?
-            redirect_to login_failure_path
-        else
-            @task = Merchant.find(session[:merchant_id]) # < recalls the value set in a previous request
-        end
-    end
-
-    def destroy
-        session.delete(:merchant_id)
-        redirect_to login_failure_path
-    end
+    redirect_to sessions_path
+  end
+  
+  def logout
+      session.delete(:user_id)
+  end
 
 end

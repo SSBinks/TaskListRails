7e4cfdba901b1aa556bb54847ac94755e23bfbca
Shari Meggs <smeggs1@gmail.com>
2016-10-23 22:46:47 -0700

    Changed root

--- TaskListRails/app/controllers/sessions_controller.rb
+++ TaskListRails/app/controllers/sessions_controller.rb
@@ -2,15 +2,15 @@ class SessionsController < ApplicationController
 
   def login_failure; end
 
-  def login; end
+  def login
+    redirect_to tasks_path;
+  end
 
   def create
     auth_hash = request.env['omniauth.auth']
     redirect_to login_failure_path unless auth_hash['uid']
     @user = User.find_by(uid: auth_hash[:uid], provider: 'github')
     if @user.nil?
-      # User doesn't match anything in the DB.
-      # Attempt to create a new user.
       @user = User.build_from_github(auth_hash)
       render :creation_failure unless @user.save
     end

ac291fb4e074f0f7fcffe3ca1701efc6792f7ff9
Shari Meggs <smeggs1@gmail.com>
2016-10-24 20:29:34 -0700

    Set up environment like gems and yml files for tests

--- TaskListRails/test/models/user_test.rb
+++ TaskListRails/test/models/user_test.rb
@@ -1,7 +1,16 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
+  test "create a userwith no email, name or uid" do
+    user = User.new
+    assert_not user.valid?
+  end
+
+  # test "the User will not be valid without an email" do
+  #   git_hash = {uid: 15, provider: "github",
+  #     info: {nickname: 'kitty'}}
+  #   user = User.build_from_github(git_hash)
+  #   assert_not user.valid?
+  #
   # end
 end

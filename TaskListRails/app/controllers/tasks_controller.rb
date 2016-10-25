f44acb677f5bcc38a1422354f934a388f93c9d72
Shari Meggs <smeggs1@gmail.com>
2016-10-24 17:39:39 -0700

    Added relationships

--- TaskListRails/app/controllers/tasks_controller.rb
+++ TaskListRails/app/controllers/tasks_controller.rb
@@ -3,7 +3,7 @@ class TasksController < ApplicationController
   before_action :require_login, except: [:index]
 
   def index
-    @tasks = Task.all
+    @tasks = @user.tasks
   end
 
   def show; end
